3cdd25f7349cadeff635b07a565c887a2590a167
Saharsh Koganti <saharshk11@gmail.com>
2025-07-10 19:59:01 -0500

    created function to process a data file

--- preprocessing/src/main.rs
+++ preprocessing/src/main.rs
@@ -8,18 +8,23 @@ struct Paper {
     target: Vec<String>
 }
 
-
 fn main() -> io::Result<()> {
     let data_path = "../data/raw/SciTLDR-AIC/dev.jsonl";
-    let file = File::open(data_path)?;
+    let test = process_file(data_path)?;
+
+    Ok(())
+}
+
+fn process_file(path: &str) -> io::Result<()>{
+    let file = File::open(path)?;                   
     let reader = BufReader::new(file);
-
-    for line_result in reader.lines() {
-        let line = line_result?;
-        let paper: Paper = serde_json::from_str(&line)?;
-        println!("{:#?}", paper);
-        break;
-    }
-
+                                                               
+    for line_result in reader.lines() { 
+        let line = line_result?;                       
+        let paper: Paper = serde_json::from_str(&line)?;       
+        println!("{:#?}", paper);                              
+        break;                                                 
+    }                                                          
+                                                               
     Ok(())
 }
