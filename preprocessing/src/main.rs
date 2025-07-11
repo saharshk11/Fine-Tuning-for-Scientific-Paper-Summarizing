0760332ef52e65a89432179a9bfb362700f169cb
Saharsh Koganti <saharshk11@gmail.com>
2025-07-11 05:02:15 -0500

    added function to find the number of tokens in a String using sentencepiece

--- preprocessing/src/main.rs
+++ preprocessing/src/main.rs
@@ -1,30 +1,46 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use serde::{Serialize, Deserialize};
+use std::io::{BufRead, BufReader};
+use std::error::Error;
+use serde::Deserialize;
+use sentencepiece::SentencePieceProcessor;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Paper {
+#[derive(Debug, Deserialize)]
+struct RawPaper {
     source: Vec<String>,
     target: Vec<String>
 }
 
-fn main() -> io::Result<()> {
-    let data_path = "../data/raw/SciTLDR-AIC/dev.jsonl";
-    let test = process_file(data_path)?;
+fn main() -> Result<(), Box<dyn Error>>{
+    let data_path = "../data/test-AIC/dev.jsonl";
+    let model_path = "models/pegasus/spiece.model";
+    process_file(data_path, model_path)?;
 
     Ok(())
 }
 
-fn process_file(path: &str) -> io::Result<()>{
-    let file = File::open(path)?;                   
+fn process_file(path: &str, model_path: &str) -> Result<(), Box<dyn Error>> {
+    let file = File::open(path)?; 
     let reader = BufReader::new(file);
-                                                               
+
     for line_result in reader.lines() { 
-        let line = line_result?;                       
-        let paper: Paper = serde_json::from_str(&line)?;       
-        println!("{:#?}", paper);                              
-        break;                                                 
-    }                                                          
+        let line = line_result?;
+        let paper: RawPaper = serde_json::from_str(&line)?;
+
+        let combined_source = paper.source.iter().fold(String::new(), |acc, val| acc + val).trim().to_string();
+        let combined_target = paper.target.iter().fold(String::new(), |acc, val| acc + val).trim().to_string();
+
+        let num_tokens = count_tokens(&combined_source, model_path)?;
+        println!("Length of text: {num_tokens}");
+    } 
                                                                
     Ok(())
+}
+
+fn count_tokens(text: &String, model_path: &str) -> Result<u32, Box<dyn Error>>{
+    let spp = SentencePieceProcessor::open(model_path)?;
+    let pieces = spp.encode(text)?;
+    let pieces = pieces.into_iter().map(|p| p.piece).collect::<Vec<_>>();
+
+
+    Ok(pieces.len() as u32)
 }
